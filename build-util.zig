65ba28f0725151564a101e4e903b8b1a55f5c9aa
MahdiGMK <mbahramiand@gmail.com>
2025-09-27 17:23:16 +0330

    initial protoc support

--- build-util.zig
+++ build-util.zig
@@ -3,6 +3,9 @@ const Alloc = std.mem.Allocator;
 const Dir = std.fs.Dir;
 const File = std.fs.File;
 const Build = std.Build;
+const Step = std.Build.Step;
+const Compile = Step.Compile;
+const LazyPath = Build.LazyPath;
 
 pub fn listDir(alloc: Alloc, root: Dir, suffix: []const u8, dirent_kind: File.Kind) ![]const []const u8 {
     var iter = root.iterate();
@@ -44,3 +47,82 @@ pub fn listFilesRecursive(alloc: Alloc, max_depth: usize, root: Dir, suffix: []c
 
     return arr.toOwnedSlice(alloc);
 }
+
+pub const ProtoC = struct {
+    step: Step,
+    protoc_cmd: ?LazyPath,
+    root_directory: LazyPath,
+    depth: usize,
+    out_langs: OutLangs,
+    // cpp_out_dir: ?LazyPath, // TODO: add support for custom paths
+    const OutLangs = packed struct {
+        cpp_out: bool,
+        const CPP = @This(){ .cpp_out = true };
+    };
+    const Options = struct { root_directory: LazyPath, depth: ?usize = null, protoc_cmd: ?LazyPath = null, out_langs: OutLangs = .CPP };
+    pub fn create(owner: *std.Build, options: Options) *ProtoC {
+        const res = owner.allocator.create(ProtoC) catch @panic("OOM");
+        res.* = ProtoC{
+            .root_directory = options.root_directory,
+            .depth = options.depth orelse 128,
+            .protoc_cmd = options.protoc_cmd,
+            .out_langs = options.out_langs,
+            .step = Step.init(.{
+                .id = .custom,
+                .owner = owner,
+                .name = "ProtoC",
+                .makeFn = ProtoC.makeFn,
+            }),
+        };
+        return res;
+    }
+    fn makeFn(step: *Step, mk_opt: std.Build.Step.MakeOptions) anyerror!void {
+        const self: *ProtoC = @fieldParentPtr("step", step);
+        const dirpath = self.root_directory.getPath3(step.owner, &self.step);
+        const protofiles = try listFilesRecursive(
+            step.owner.allocator,
+            self.depth,
+            try dirpath.root_dir.handle.openDir("./", .{ .iterate = true }),
+            ".proto",
+        );
+
+        const node = mk_opt.progress_node.start("protoc", protofiles.len);
+        defer node.end();
+
+        for (protofiles) |file| {
+            defer node.completeOne();
+
+            const cwd = try dirpath.root_dir.handle.realpathAlloc(step.owner.allocator, "./");
+            const protoc_cmd =
+                if (self.protoc_cmd) |cmd| blk: {
+                    const path = cmd.getPath3(step.owner, step);
+                    break :blk try path.root_dir.handle.realpathAlloc(step.owner.allocator, path.sub_path);
+                } else "protoc";
+
+            var argv = std.ArrayList([]const u8).empty;
+            try argv.appendSlice(step.owner.allocator, &.{ protoc_cmd, file });
+            defer argv.deinit(step.owner.allocator);
+
+            if (self.out_langs.cpp_out)
+                try argv.append(step.owner.allocator, "--cpp_out=./"); // TODO: add support for custom paths
+
+            try step.handleChildProcUnsupported(cwd, argv.items);
+            const res = try std.process.Child.run(.{
+                .allocator = step.owner.allocator,
+                .argv = argv.items,
+                .cwd = cwd,
+                .progress_node = node,
+            });
+            step.handleChildProcessTerm(res.term, cwd, argv.items) catch |e| {
+                switch (e) {
+                    error.MakeFailed => try step.addError(
+                        \\protoc command failed :
+                        \\{s}
+                    , .{res.stderr}),
+                    else => {},
+                }
+                return e;
+            };
+        }
+    }
+};
